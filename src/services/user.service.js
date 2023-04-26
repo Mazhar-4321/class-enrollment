ea698dabdf901ca3db7f2069050cc76159bb951e
Mazhar-4321 <syedmazharali742@gmail.com>
2023-04-27 01:55:28 +0530

    [Mazhar]ADDED: Logic To Get User Details Using Email

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -41,3 +41,33 @@ export const newUser = async (body) => {
     throw new Error('Invalid OTP')
   }
 };
+
+export const getUser = async (body) => {
+  const { QueryTypes } = require('sequelize');
+
+  var response = await sequelize.query(
+    `select U.firstName,U.lastName,U.email,U.password,R.role_name from users U
+  inner join
+  role R
+  on U.email=R.email and U.email=? ;`,
+    {
+      replacements: [body.email],
+      type: QueryTypes.SELECT
+    }
+  );
+  console.log("response from db", response)
+  if (response.length > 0) {
+    const verified = bcrypt.compareSync(body.password, response[0].password)
+    if (!verified) {
+      logger.error(`Attempt To Login With Invalid Password ${body.email}`)
+      throw new Error('Invalid Password')
+    } else {
+      var token = await Jwt.sign({ email: body.email }, process.env.SECRET_KEY);
+      console.log("token", token)
+      return response[0].role_name + "," + token
+    }
+  } else {
+
+  }
+  return response;
+};
